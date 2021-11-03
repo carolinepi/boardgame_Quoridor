d05bb46022dcf0d465d930d8a24f3817d07f44ac
Dina Iovcheva <d.iovcheva@itransition.com>
2021-11-03 23:41:48 +0200

    shortest way from position to destination

--- controllers/dijkstra.py
+++ controllers/dijkstra.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple, List
 
 from models.grid_position import GridPosition
 
@@ -29,3 +29,46 @@ def calculate_path_from_position(
         unvisited.pop(current)
 
     return False
+
+
+def calculate_shortest_path_from_position(
+        matrix: Dict[GridPosition, Dict[GridPosition, int]],
+        position: GridPosition,
+        destination: GridPosition
+) -> Tuple[List[GridPosition], int]:
+    visited = {position: 0, }
+    current = position
+    unvisited = {grid: float('inf') for grid in matrix if grid != position}
+    previous_nodes = {}
+    path = []
+
+    while unvisited:
+        for grid in matrix[current]:
+            if grid not in visited:
+                if unvisited[grid] == float('inf'):
+                    unvisited[grid] = visited[current] + matrix[current][grid]
+                    previous_nodes[grid] = current
+                elif unvisited[grid] >= unvisited[grid] + matrix[current][grid]:
+                    unvisited[grid] += matrix[current][grid]
+                    previous_nodes[grid] = current
+
+        current = min(unvisited, key=unvisited.get)
+        visited[current] = unvisited[current]
+        unvisited.pop(current)
+
+    grid = destination
+    while grid != position:
+        path.append(grid)
+        if grid in previous_nodes:
+            grid = previous_nodes[grid]
+        else:
+            path = []
+            break
+
+    if path:
+        path.append(position)
+        path.reverse()
+
+    return path, visited[destination]
+
+
